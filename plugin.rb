6a8a94d19197e0260ecbac43a58af476755cbd08
zhangml123 <s15532292192@163.com>
2020-03-05 17:54:17 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -11,20 +11,4 @@ enabled_site_setting :spoiler_enabled
 register_asset "javascripts/spoiler.js"
 register_asset "stylesheets/discourse_spoiler_alert.css"
 
-after_initialize do
 
-  on(:reduce_cooked) do |fragment, post|
-    fragment.css(".spoiler").each do |el|
-      link = fragment.document.create_element("a")
-      link["href"] = post.url
-      link.content = I18n.t("spoiler_alert.excerpt_spoiler")
-      el.inner_html = link.to_html
-    end
-  end
-
-  # Remove spoilers from topic exerpts
-  on(:reduce_excerpt) do |doc, post|
-    doc.css(".spoiler").remove
-  end
-
-end
